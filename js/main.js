71f579375b4641546ba0ebd98ee291015a934793
2joopiter <veryberrylike3@gmail.com>
2023-11-17 13:18:04 +0900

    youtube 데이터 호출 및 동적 DOM 생성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 const api_key = 'AIzaSyBCqSk4zIkhEpjHpiWBU8U5ZbT7HiSItqc';
 const playlist_id = 'PL7mCE55Wm-djdbvuzIWO64d3eCVtnYPa0';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&api_key=${api_key}&playlistId=${playlist_id}maxResults=6;`;
+const num = 7;
+const result_url = `${base_url}?part=snippet&key=${api_key}&playlistId=${playlist_id}&maxResults=${num}`;
+
+fetch(result_url)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+		json.items.forEach((el) => {
+			tags += `
+      <article>
+        <img src=${el.snippet.thumbnails.standard.url} />
+        <h2>${el.snippet.title}</h2>
+        <p>${el.snippet.description}</p>
+        <span>${el.snippet.publishedAt}</span>
+      </article>
+      `;
+		});
+		document.body.innerHTML = tags;
+	});
 
 /*
   브라우저 URL을 통해서 데이터를 요청하는 방식을 GET 방식이라고 함
